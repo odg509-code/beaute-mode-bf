499ef587aac1d26ef703df9e4a8e94f969906e32
Guélilou <ton-email@exemple.com>
2026-08-17 23:21:26 +0000

    Nouveau layout accueil style marketplace + carrousel banner + mises à jour diverses

--- beauty/urls.py
+++ beauty/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
 from . import views
-urlpatterns = [path('', views.institutes, name='institutes'), path('<slug:slug>/reserver/', views.book, name='book')]
+urlpatterns = [path('', views.institutes, name='institutes'),
+              path('<slug:slug>/reserver/', views.book, name='book')
+              ]
+
